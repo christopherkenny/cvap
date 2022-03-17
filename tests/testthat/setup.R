6806e27f0950b1dcce9e346f7e74d768706f6746
Christopher Kenny <ctk44@cornell.edu>
2022-03-17 16:18:15 -0400

    fix tests

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,4 +1,4 @@
 state <- 'DE'
-cvap <- cvap_get(state)
+cvap <- cvap_get(state, year = 2019)
 data('de_block')
 data('de_block_group')
--- tests/testthat/test-get.R
+++ tests/testthat/test-get.R
@@ -1,9 +1,9 @@
 test_that("cvap_get default works", {
-  de <- cvap_get('DE')
+  de <- cvap_get('DE', year = 2019)
   expect_equal(sum(de$cvap), 710270)
 })
 
 test_that("cvap_get other geography works", {
-  de <- cvap_get('DE', geography =  'block group')
+  de <- cvap_get('DE', geography =  'block group', year = 2019)
   expect_equal(sum(de$cvap), 710270)
 })
